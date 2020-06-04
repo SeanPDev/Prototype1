3575866cb35b0ca37e48b9793d00672dd874608c
Sean <SeanPDev@gmail.com>
2020-06-04 06:34:36 -0400

    Using deltatime

--- PlayerController.cs
+++ PlayerController.cs
@@ -15,6 +15,7 @@ public class PlayerController : MonoBehaviour
     {
         // Move the vehicle forward
         //transform.Translate(0, 0, 1);
-        transform.Translate(Vector3.forward);
+        transform.Translate(Vector3.forward * Time.deltaTime * 20);
+
     }
 }
